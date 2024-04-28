d2956eca37743366f13a968a807090e3395b211d
Rishabh <rishabhgoel2020@gmail.com>
2024-04-28 23:34:30 +0530

    Add styling to the NavBar using styled component:

--- movie_project/src/Navbar.js
+++ movie_project/src/Navbar.js
@@ -1,14 +1,65 @@
+import styled from "styled-components";
+
+//this is a STYLED COMPONENT that has replaced div
+const Nav = styled.div`
+  box-sizing: border-box;
+  color: white;
+  height: 50px;
+  overflow: hidden;
+  width: 100%;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    149deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 41%,
+    rgba(0, 212, 255, 1) 100%
+  );
+
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const Cart = styled.div`
+  box-sizing: border-box;
+  cursor: Pointer;
+  position: relative;
+`;
+
+const CartImg = styled.img`
+  height: 40px;
+`;
+
+const CartSpan = styled.span`
+  position: absolute;
+  background-color: green;
+  width: 15px;
+  text-align: center;
+  border-radius: 100%;
+  right: -1px;
+  top: -2px;
+`;
+
 function NavBar() {
-    return (
-        <>
-            <div>
-                <div><h1>NavBar</h1></div>
-                <div>
-                    <img src="https://cdn-icons-png.flaticon.com/128/2838/2838895.png" alt="Cart"/>
-                    <span>0</span>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Nav>
+        <div>
+          <Title>NavBar</Title>
+        </div>
+        <Cart>
+          <CartImg
+            src="https://cdn-icons-png.flaticon.com/128/2838/2838895.png"
+            alt="Cart"
+          />
+          <CartSpan>0</CartSpan>
+        </Cart>
+      </Nav>
+    </>
+  );
 }
-export default NavBar
+export default NavBar;
